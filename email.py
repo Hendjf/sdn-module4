e42911b962351b8860e6bae59f39685038e3d8f0
“SwitchFiasco” <EdwinRaySalgado@gmail.com>
2020-07-24 02:38:57 -0400

    Exposition Added By Way Of Comments

--- email.py
+++ email.py
@@ -7,9 +7,9 @@ a list and prints the list.
 
 
 addresses = []
-
+#code above is where the recorded addresses are displayed showing which are current
 more = input("Do you have an email address to enter (y/n)? ")
-
+#prompts user to confirm whether or not an address needs to be added
 while more == "y":
     email = input("Enter the address: ")
     addresses.append(email)
@@ -19,5 +19,6 @@ while more == "y":
             break
         else:
             more = input("Please enter a y or n: ")
-    
+#this portion of code allows yuo to add more than one email if there are multiple to add
 print(addresses)
+#this displays the updated list of addresses
