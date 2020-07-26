dbdd85f4f094387a3b6cb964bfd88d4b128f62e5
rhettarcher <alex@archer.family>
2020-07-26 12:48:35 -0400

    Added comments regarding command purpose

--- email.py
+++ email.py
@@ -7,9 +7,10 @@ a list and prints the list.
 
 
 addresses = []
-
+#This is prompting the user for information, specifically if they have an email or not
 more = input("Do you have an email address to enter (y/n)? ")
 
+#This is prompting the user to enter a string of additional email address, it appears as though it would create some kind of table
 while more == "y":
     email = input("Enter the address: ")
     addresses.append(email)
@@ -19,5 +20,6 @@ while more == "y":
             break
         else:
             more = input("Please enter a y or n: ")
-    
+
+#This command prints the addresses in said table    
 print(addresses)
