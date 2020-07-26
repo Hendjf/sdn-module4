af800283a5c46af1a950859fd55c7a171cad927a
AARON117117 <A2ron117@gmail.com>
2020-07-26 12:48:32 -0400

    Added comments for information regarding email promrpt script

--- email.py
+++ email.py
@@ -5,11 +5,11 @@ a list and prints the list.
 
 """
 
-
+#This is equal to the email addresses entered
 addresses = []
-
+#This is a prompt for a user to answer if they have an email address to enter or not
 more = input("Do you have an email address to enter (y/n)? ")
-
+#If the user selected Yes, or Y, they must now enter an email address, then once an address is entered they have the option to enter a secondary address if they so choose. If no, then they do not enter an address.
 while more == "y":
     email = input("Enter the address: ")
     addresses.append(email)
@@ -21,3 +21,4 @@ while more == "y":
             more = input("Please enter a y or n: ")
     
 print(addresses)
+#^ This command prints the entered addresses.
