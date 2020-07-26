85e1f06a965f76ddc86247656aaa3eb2b7d952eb
levekl1 <leveillek94@gmail.com>
2020-07-25 22:07:19 -0400

    Added insight on email.py file

--- email.py
+++ email.py
@@ -5,11 +5,13 @@ a list and prints the list.
 
 """
 
-
+##appears to be creating a list of addresses using addresses = input
 addresses = []
 
+##assigns the value yes/no to more
 more = input("Do you have an email address to enter (y/n)? ")
 
+##creating a loop, while "yes" is true, a new email address will be added until the loop hits "n"
 while more == "y":
     email = input("Enter the address: ")
     addresses.append(email)
@@ -19,5 +21,6 @@ while more == "y":
             break
         else:
             more = input("Please enter a y or n: ")
-    
+
+##prints the list of addresses that was fed into the list    
 print(addresses)
